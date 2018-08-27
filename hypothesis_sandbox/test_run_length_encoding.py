b163534f9fb373ed2e2e0222077f4b0e13131c55
Paul Amazona <netfiddler@yahoo.com>
2018-08-27 13:59:47 +1000

    green version

---
+++ hypothesis_sandbox/test_run_length_encoding.py
@@ -0,0 +1,19 @@
+﻿import unittest
+
+from run_length_encoding import *
+
+from hypothesis import given, example
+from hypothesis.strategies import text
+
+class Test_RunLengthEncoding(unittest.TestCase):
+    @given(s=text())
+    @example(s='')
+    def test_decode_inverts_encode(self, s):
+        self.assertEqual(decode(encode(s)), s)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
